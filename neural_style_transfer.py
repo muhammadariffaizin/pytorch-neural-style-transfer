8bca47eaec3bfb03e0092cbf2562e63e469924c3
wiratmo <4a.wiratmo@gmail.com>
2024-07-08 14:11:44 +0700

    feat: adding inpaint module

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -14,7 +14,7 @@ def gram_matrix(tensor):
     b, c, h, w = tensor.size()
     features = tensor.view(b, c, h * w)
     G = torch.bmm(features, features.transpose(1, 2))
-    return G.div(c * h * w)
+    return G.div(h * w)
 
 def rgb_to_grayscale(img):
     if img.dim() != 3 or img.size(0) != 3:
@@ -23,7 +23,7 @@ def rgb_to_grayscale(img):
     gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
     return gray.unsqueeze(0)
 
-def build_loss(neural_net, optimizing_img, mrf_layers, loss_layers, loss_fns, targets, weights, style_layers, style_patches_lists, weight_list, patch_size, dist_template, cont_dist, mrf_loss_fn, config, device):
+def build_loss(neural_net, optimizing_img, mrf_layers, loss_layers, loss_fns, targets, weights, style_layers, style_patches_lists, weight_list, patch_size, dist_template, inpaint_template, cont_dist, mrf_loss_fn, config, device):
     optimizing_fms = neural_net(optimizing_img, loss_layers)
 
     content_loss = 0
@@ -42,19 +42,24 @@ def build_loss(neural_net, optimizing_img, mrf_layers, loss_layers, loss_fns, ta
     d_temp = cont_dist * optimizing_img.clone()
     dist_loss = torch.nn.MSELoss().to(device)(d_temp, dist_template)
 
+    # Inpainting loss
+    i_temp = utils.inpaint_cv2(optimizing_img.clone(), device)
+    color_loss = torch.nn.MSELoss().to(device)(i_temp.mean(dim=[2,3]), inpaint_template.mean(dim=[2,3]))
+
     content_loss *= config['content_weight']
     style_loss *= config['style_weight']
     mrf_loss *= config['mrf_weight']
     dist_loss *= config['dist_weight']
+    color_loss *= config['color_weight']
 
     # Add L2 regularization
     l2_reg = torch.tensor(0., requires_grad=True).to(optimizing_img.device)
     for param in neural_net.parameters():
         l2_reg += torch.norm(param)
     
-    total_loss = content_loss + style_loss + mrf_loss + dist_loss + 0.001*l2_reg
+    total_loss = content_loss + style_loss + mrf_loss + dist_loss
 
-    return total_loss, content_loss, style_loss, mrf_loss, dist_loss
+    return total_loss, content_loss, style_loss, mrf_loss, dist_loss, color_loss
 
 def neural_style_transfer(config):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -69,6 +74,10 @@ def neural_style_transfer(config):
     content_img = utils.prepare_img(content_img_path, config['height'], device, config['content_invert'])
     style_img = utils.prepare_img(style_img_path, config['height'], device, config['style_invert'])
 
+    # test save image
+    utils.save_image_with_unnormalize_img(content_img, 'content.jpg')
+    utils.save_image_with_unnormalize_img(style_img, 'style.jpg')
+
     if config['init_method'] == 'random':
         gaussian_noise_img = np.random.normal(loc=0, scale=90., size=content_img.shape).astype(np.float32)
         gaussian_noise_img = torch.from_numpy(gaussian_noise_img).to(device)
@@ -81,16 +90,17 @@ def neural_style_transfer(config):
 
     # Define style layers
     style_layers = ['relu1_1','relu2_1','relu3_1','relu4_1','relu5_1']
-    style_weights = [1e3/n**3 for n in [64,128,256,512,512]]
+    style_weights = [1e4/n**3 for n in [64,128,256,512,512]]
     # Define mrf layers
     mrf_layers = ['relu3_1', 'relu4_1'] 
-    # Defince content layers
+    # Define content layers
     content_layers = ['relu4_2']
-    content_weights = [1e5]
+    content_weights = [1e3]
     # loss layers: layers to be used by opt_img ( style_layers & mrf_layers & content_layers)
     loss_layers = mrf_layers + style_layers + content_layers
 
     neural_net = utils.prepare_model(config['model'])
+    neural_net.load_state_dict(torch.load('vgg_conv.pth'))
     neural_net.to(device)
     print(f'Using {config["model"]} in the optimization procedure.')
 
@@ -121,14 +131,21 @@ def neural_style_transfer(config):
     cont_dist[cont_dist==float("Inf")] = 1e3
     dist_template = cont_dist*content_img
 
-    cv.imwrite('dist_template.png', np.transpose(dist_template.to('cpu').detach().numpy().squeeze(axis=0), (1, 2, 0)))
+    # create inpaint module path
+    inpaint_img = utils.inpaint_cv2(style_img.clone(), device)
+    print(f'Input image shape: {inpaint_img.shape}')
+    print(f'Content image shape: {content_img.shape}')
+    inpaint_template = inpaint_img*(255 - content_img.clone())
+
+    utils.save_image_with_unnormalize_img(dist_template, 'dist_template.png')
+    utils.save_image_with_unnormalize_img(inpaint_template, 'inpaint_template.png')
 
     num_of_iterations = {'lbfgs': 1000, 'adam': 2000}
 
     if config['optimizer'] == 'adam':
         optimizer = Adam([optimizing_img], lr=1e1)
         for cnt in range(num_of_iterations[config['optimizer']]):
-            total_loss, content_loss, style_loss, mrf_loss, dist_loss = build_loss(neural_net, optimizing_img, mrf_layers, loss_layers, loss_fns, targets, weights, style_layers, style_patches_lists, weight_list, config['patch_size'], dist_template, cont_dist, mrf_loss_fn, config, device)
+            total_loss, content_loss, style_loss, mrf_loss, dist_loss, color_loss = build_loss(neural_net, optimizing_img, mrf_layers, loss_layers, loss_fns, targets, weights, style_layers, style_patches_lists, weight_list, config['patch_size'], dist_template, inpaint_template, cont_dist, mrf_loss_fn, config, device)
 
             total_loss.backward()
             # Clip gradients
@@ -138,7 +155,7 @@ def neural_style_transfer(config):
             optimizer.zero_grad()
 
             with torch.no_grad():
-                print(f'Adam | iteration: {cnt:03}, total loss={total_loss.item():12.4f}, content_loss={config["content_weight"] * content_loss.item():12.4f}, style loss={config["style_weight"] * style_loss.item():12.4f}, mrf loss={config["mrf_weight"] * mrf_loss.item():12.4f}, dist loss={config["dist_weight"] * dist_loss.item():12.4f}')
+                print(f'Adam | iteration: {cnt:03}, total loss={total_loss.item():12.4f}, content_loss={config["content_weight"] * content_loss.item():12.4f}, style loss={config["style_weight"] * style_loss.item():12.4f}, mrf loss={config["mrf_weight"] * mrf_loss.item():12.4f}, dist loss={config["dist_weight"] * dist_loss.item():12.4f}, color loss={config["color_weight"] * color_loss.item():12.4f}')
                 utils.save_and_maybe_display(optimizing_img, dump_path, config, cnt, num_of_iterations[config['optimizer']], should_display=False)
     elif config['optimizer'] == 'lbfgs':
         optimizer = LBFGS([optimizing_img], max_iter=num_of_iterations['lbfgs'], line_search_fn='strong_wolfe')
@@ -148,14 +165,14 @@ def neural_style_transfer(config):
             nonlocal cnt
             if torch.is_grad_enabled():
                 optimizer.zero_grad()
-            total_loss, content_loss, style_loss, mrf_loss, dist_loss = build_loss(neural_net, optimizing_img, mrf_layers, loss_layers, loss_fns, targets, weights, style_layers, style_patches_lists, weight_list, config['patch_size'], dist_template, cont_dist, mrf_loss_fn, config, device)
+            total_loss, content_loss, style_loss, mrf_loss, dist_loss, color_loss = build_loss(neural_net, optimizing_img, mrf_layers, loss_layers, loss_fns, targets, weights, style_layers, style_patches_lists, weight_list, config['patch_size'], dist_template, inpaint_template, cont_dist, mrf_loss_fn, config, device)
             if total_loss.requires_grad:
                 total_loss.backward()
 
                 # Clip gradients
                 torch.nn.utils.clip_grad_norm_([optimizing_img], max_norm=1.0)
             with torch.no_grad():
-                print(f'L-BFGS | iteration: {cnt:03}, total loss={total_loss.item():12.4f}, content_loss={config["content_weight"] * content_loss.item():12.4f}, style loss={config["style_weight"] * style_loss.item():12.4f}, mrf loss={config["mrf_weight"] * mrf_loss.item():12.4f}, dist loss={config["dist_weight"] * dist_loss.item():12.4f}')
+                print(f'L-BFGS | iteration: {cnt:03}, total loss={total_loss.item():12.4f}, content_loss={config["content_weight"] * content_loss.item():12.4f}, style loss={config["style_weight"] * style_loss.item():12.4f}, mrf loss={config["mrf_weight"] * mrf_loss.item():12.4f}, dist loss={config["dist_weight"] * dist_loss.item():12.4f}, color loss={config["color_weight"] * color_loss.item():12.4f}')
                 utils.save_and_maybe_display(optimizing_img, dump_path, config, cnt, num_of_iterations[config['optimizer']], should_display=False)
             cnt += 1
             return total_loss
@@ -176,8 +193,9 @@ if __name__ == '__main__':
     parser.add_argument("--height", type=int, help="height of the content and style images, aspect ratio preserved", default=500)
     parser.add_argument("--content_weight", type=float, help="weight factor for content loss", default=3e4)
     parser.add_argument("--style_weight", type=float, help="weight factor for style loss", default=1e5)
-    parser.add_argument("--mrf_weight", type=float, help="weight factor for mrf loss", default=1e2)
-    parser.add_argument("--dist_weight", type=float, help="weight factor for distance loss", default=1e3)
+    parser.add_argument("--mrf_weight", type=float, help="weight factor for mrf loss", default=1e3)
+    parser.add_argument("--dist_weight", type=float, help="weight factor for distance loss", default=1e6)
+    parser.add_argument("--color_weight", type=float, help="weight factor for color loss", default=1e2)
 
     parser.add_argument("--patch_size", type=int, help="patch size of the style image", default=5)
     parser.add_argument("--optimizer", type=str, choices=['lbfgs', 'adam'], default='lbfgs')
--- utils/utils.py
+++ utils/utils.py
@@ -30,26 +30,53 @@ def load_image(img_path, target_shape=None, invert=False):
     # this need to go after resizing - otherwise cv.resize will push values outside of [0,1] range
     img = img.astype(np.float32)  # convert from uint8 to float32
     img /= 255.0  # get to [0, 1] range
+
+    if invert:
+        img = 1.0 - img
     return img
+
+# normalize using ImageNet's mean
+normalize_img = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Lambda(lambda x: x.mul(255)),
+    transforms.Normalize(mean=IMAGENET_MEAN_255, std=IMAGENET_STD_NEUTRAL)
+])
 
 def prepare_img(img_path, target_shape, device, invert=False):
     img = load_image(img_path, target_shape=target_shape, invert=invert)
 
-    # normalize using ImageNet's mean
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Lambda(lambda x: x.mul(255)),
-        transforms.Normalize(mean=IMAGENET_MEAN_255, std=IMAGENET_STD_NEUTRAL)
-    ])
-
-    img = transform(img).to(device).unsqueeze(0)
+    img = normalize_img(img).to(device).unsqueeze(0)
 
     return img
+
+def unnormalize_img(img):
+    dump_img = img.squeeze(axis=0).to('cpu').detach().numpy()
+    dump_img = np.moveaxis(dump_img, 0, 2)  # swap channel from 1st to 3rd position: ch, _, _ -> _, _, chr
+    dump_img = np.copy(dump_img)
+    dump_img += np.array(IMAGENET_MEAN_255).reshape((1, 1, 3))
+    dump_img = np.clip(dump_img, 0, 255).astype('uint8')
+    return dump_img
 
 def save_image(img, img_path):
     if len(img.shape) == 2:
         img = np.stack((img,) * 3, axis=-1)
     cv.imwrite(img_path, img[:, :, ::-1])  # [:, :, ::-1] converts rgb into bgr (opencv constraint...)
+
+def save_image_with_unnormalize_img(img, img_path):
+    # check if img is a tensor
+    if isinstance(img, torch.Tensor):
+        if len(img.shape) == 4:
+            img = img.squeeze(axis=0) # (1, C, H, W) -> (C, H, W)
+        elif len(img.shape) == 2:
+            img = img.unsqueeze(axis=0) # (H, W) -> (1, H, W)
+        img = img.to('cpu').detach().numpy()
+    img = np.moveaxis(img, 0, 2)  # swap channel from 1st to 3rd position: ch, _, _ -> _, _, chr
+
+    dump_img = np.copy(img)
+    dump_img += np.array(IMAGENET_MEAN_255).reshape((1, 1, 3))
+    dump_img = np.clip(dump_img, 0, 255).astype('uint8')
+    dump_img = dump_img[:, :, ::-1] # convert bgr to rgb
+    cv.imwrite(img_path, dump_img)
 
 def generate_out_img_name(config):
     prefix = os.path.basename(config['content_img_name']).split('.')[0] + '_' + os.path.basename(config['style_img_name']).split('.')[0]
@@ -61,21 +88,17 @@ def generate_out_img_name(config):
 
 def save_and_maybe_display(optimizing_img, dump_path, config, img_id, num_of_iterations, should_display=False):
     saving_freq = config['saving_freq']
-    out_img = optimizing_img.squeeze(axis=0).to('cpu').detach().numpy()
-    out_img = np.moveaxis(out_img, 0, 2)  # swap channel from 1st to 3rd position: ch, _, _ -> _, _, chr
 
     # for saving_freq == -1 save only the final result (otherwise save with frequency saving_freq and save the last pic)
     if img_id == num_of_iterations-1 or (saving_freq > 0 and img_id % saving_freq == 0):
         img_format = config['img_format']
         out_img_name = str(img_id).zfill(img_format[0]) + img_format[1] if saving_freq != -1 else generate_out_img_name(config)
-        dump_img = np.copy(out_img)
-        dump_img += np.array(IMAGENET_MEAN_255).reshape((1, 1, 3))
-        dump_img = np.clip(dump_img, 0, 255).astype('uint8')
+        dump_img = unnormalize_img(optimizing_img)
         cv.imwrite(os.path.join(dump_path, out_img_name), dump_img[:, :, ::-1])
 
-    if should_display:
-        plt.imshow(np.uint8(get_uint8_range(out_img)))
-        plt.show()
+        if should_display:
+            plt.imshow(np.uint8(get_uint8_range(dump_img)))
+            plt.show()
 
 def get_uint8_range(x):
     if isinstance(x, np.ndarray):
@@ -131,22 +154,45 @@ def rgb_to_grayscale(img):
     return gray.unsqueeze(0)
 
 def dist_cv2(img, device):
-    img = img.squeeze(axis=0)
-    # convert dimension (3, 256, 256) to (256, 256, 3)
-    img = np.transpose(img.to('cpu').detach().numpy(), (1, 2, 0))
+    img = unnormalize_img(img)
     img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
     img = cv.blur(img, (1, 1))
-    img = np.asarray(img, dtype=np.uint8)
+    # img = np.asarray(img, dtype=np.uint8)
     img = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
+    save_image(img, 'test-1.png')
     img = np.asarray(img, dtype=np.uint8)
-    cv.imwrite('test-1.png', img)
     img = ndimage.grey_erosion(img, size=(3, 3))
 
-    img_dist = cv.distanceTransform(img, cv.DIST_L2, 5)
-    cont_dist = torch.from_numpy(img_dist).float().to(device)
+    img_dist = cv.distanceTransform(img, cv.DIST_L2, 3)
+    save_image(img_dist, 'test-1-2.png')
+    img_dist = cv.cvtColor(img_dist, cv.COLOR_GRAY2RGB) # (256, 256) -> (256, 256, 3)
+    img_dist = normalize_img(img_dist) # (256, 256, 3) -> (3, 256, 256)
+    cont_dist = img_dist.float().to(device)
 
     f = cont_dist.unsqueeze(0)
-    a = torch.cat([f, f, f], 0)
-    cv.imwrite('test-2.png', np.transpose(a.to('cpu').detach().numpy() * 255, (1, 2, 0)))
-    a = a.unsqueeze(0)
-    return a
+    save_image_with_unnormalize_img(f, 'test-2.png') # (1, 3, 256, 256)
+    return f
+
+def inpaint_cv2(img, device):
+    img = unnormalize_img(img)
+
+    # create mask from image using thresholding
+    mask = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+    mask = cv.blur(mask, (3, 3))
+    mask = cv.threshold(mask, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
+    mask = 255 - mask
+    save_image(mask, 'test-3.png')
+
+    # inpaint image
+    save_image(img, 'test-3-2.png')
+    img_inpaint = cv.inpaint(img, mask, 3, cv.INPAINT_TELEA)
+    save_image(img_inpaint, 'test-4-1.png')
+    # img_inpaint = np.moveaxis(img_inpaint, 0, 1)  # swap channel from 1st to 3rd position: _, _, ch -> ch, _, _
+    img_inpaint = normalize_img(img_inpaint) # (256, 256, 3) -> (3, 256, 256)
+    # img_inpaint = np.moveaxis(img_inpaint, -1, 0)  # (256, 256, 3) -> (3, 256, 256)
+    cont_dist = img_inpaint.float().to(device)
+
+    # cv.imwrite('test-4.png', cont_dist.to('cpu').detach().numpy())
+    f = cont_dist.unsqueeze(0)
+    save_image_with_unnormalize_img(f, 'test-4.png') # (1, 256, 256, 3)
+    return f
